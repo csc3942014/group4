b6ce3274ef2badac8d440a3d7212179279c967c0
csc3942014 <csc3942014@gmail.com>
2014-05-15 01:14:22 +0000

    Added test manager page (again)

---
+++ app/controllers/test_manager_controller.rb
@@ -0,0 +1,7 @@
+class TestManagerController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'landing_page/index',		:as => :landing_page
 
+  get 'test_manager/index',		:as => :test_manager
   get 'testing/index', 			:as => :testing
   get 'test_setup/index', 		:as => :test_setup
   get 'analytics_comparatif/index', 	:as => :analytics_comparatif
