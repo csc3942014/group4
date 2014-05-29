22ff54c7ebeda36494ce81b2124e1f823bdbf24c
csc3942014 <csc3942014@gmail.com>
2014-05-29 21:08:35 +0000

    Addtl work on General Analytics Page and controller.

--- app/controllers/analytics_general_controller.rb
+++ app/controllers/analytics_general_controller.rb
@@ -2,5 +2,12 @@ class AnalyticsGeneralController < ApplicationController
   def index
       @kbrds = Keyboard.all;
       @ts = TestSuite.all;
+      
+      ary = Array.new;
+      for @kb in @kbrds
+          for @t in @ts
+              ary[@kb.id][@t.id] = TestSession.where("test_suite_id = @t.id AND keyboard_id = @kb.id");
+          end
+      end
   end
 end
