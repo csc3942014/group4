cc3b52d56dfe0b240eabaf1936f91550beb568d6
csc3942014 <csc3942014@gmail.com>
2014-06-04 01:13:47 +0000

    Added subfilters to Specific Analytics Page

---
+++ app/assets/javascripts/analytics_specific.js
@@ -0,0 +1,37 @@
+function loadKeyboardFilter()
+{
+    $( ".filterTbl" ).load( "/analytics_specific/reload_keyboards", {});
+};
+
+function loadTestsuiteFilter()
+{
+    $( ".filterTbl" ).load( "/analytics_specific/reload_testsuites", {});
+};
+
+function loadUserFilter()
+{
+    $( ".filterTbl" ).load( "/analytics_specific/reload_users", {});
+};
+
+
+function loadKeyboardData(row, kbrdId)
+{
+    setSelectedRow(row);
+    
+    //$( ".wordsTbl" ).load( "/test_manager/reload_words", { test_suite_id: suiteId } );
+    
+};
+
+function cleanRowStyles()
+{
+    var table = document.getElementById("theTbl");
+    for (var i = 0, row; row = table.rows[i]; i++) {
+       $(row).removeClass('success'); 
+    }
+};
+
+function setSelectedRow(row)x
+{
+    cleanRowStyles();
+    $(row).parent('tr').addClass('success');
+};
