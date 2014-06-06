d605d02063ba0374a35761d83d6e3f6cb62efd33
csc3942014 <csc3942014@gmail.com>
2014-06-06 21:00:27 +0000

    Further work on the specific - filter by keyboard page

--- app/controllers/analytics_specific_controller.rb
+++ app/controllers/analytics_specific_controller.rb
@@ -21,8 +21,49 @@ class AnalyticsSpecificController < ApplicationController
   end
     
   def loadup_keyboard
-      @keyboard = Keyboard.where('id = ?', params[:keyboard_id].to_i);
-      render :partial => "open_keyboard_stats", object: @keyboard
+      
+      #get access to the selected keyboard
+      @keyboard = Keyboard.where('id = ?', params[:keyboard_id].to_i)
+      #get all test sessions performed for this keyboard
+      @testSessions = TestSession.where('keyboard_id = ?', params[:keyboard_id].to_i)
+      
+      #declare the hashtable for the pie chart data
+      @pieChartData = {'Correct' => 0, 'Incorrect' => 0}
+      @durationChartData = {'0' => 0.0, '1' => 0.0, '2' => 0.0, '3' => 0.0, '4' => 0.0, '5' => 0.0, '6' => 0.0, '7' => 0.0, '8' => 0.0, '9' => 0.0, '10+' => 0.0}
+      @durationChartFrequency = {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0, '7' => 0, '8' => 0, '9' => 0, '10+' => 0}
+      
+      #generate chart data
+      @testSessions.each do |tSess|
+          #get all the test units for this particular test session
+          @allTU = TestUnit.where('test_session_id = ?', tSess.id)
+          @allTU.each do |tu|
+              #get the word for this test unit
+              @word = Word.where('id = ?', tu.word_id)
+              @word.each do |wrd|
+                  #if the user typed the word correctly, increment correct value in hashtable, increment incorrect otherwise
+                  if tu.input === wrd.word
+                      @pieChartData['Correct'] += 1
+                  else
+                      @pieChartData['Incorrect'] += 1
+                  end
+                  
+                  #add duration to durationChartData
+                  duration = tu.end_time - tu.start_time
+                  if wrd.length < 10 && wrd.length > 0
+                      @durationChartData[wrd.length.to_s] += duration
+                  else 
+                      @durationChartData['10+'] += duration
+                  end 
+              end
+          end
+      end
+      #turn totals into averages for the duration chart
+      @durationChartData.each do |key, duration|
+          @durationChartData[key] = ( @durationChartData[key] / @durationChartFrequency[key] )
+      end
+     
+      
+      render :partial => "open_keyboard_stats", :locals => { :pcData => @pieChartData, :keyboard => @keyboard, :dcData => @durationChartData } 
   end
-    
+
 end
