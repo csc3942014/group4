e0143555693f3249f9abc318a834795ce4acbb70
csc3942014 <csc3942014@gmail.com>
2014-06-07 12:31:51 +0000

    Nearly finished content loading for specific analytics page by keyboard

--- app/controllers/analytics_specific_controller.rb
+++ app/controllers/analytics_specific_controller.rb
@@ -31,9 +31,19 @@ class AnalyticsSpecificController < ApplicationController
       @pieChartData = {'Correct' => 0, 'Incorrect' => 0}
       @durationChartData = {'0' => 0.0, '1' => 0.0, '2' => 0.0, '3' => 0.0, '4' => 0.0, '5' => 0.0, '6' => 0.0, '7' => 0.0, '8' => 0.0, '9' => 0.0, '10+' => 0.0}
       @durationChartFrequency = {'0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0, '7' => 0, '8' => 0, '9' => 0, '10+' => 0}
+      @userPercepData = [0,0,0]
+      @userPercepFrequency = [0,0,0]
       
       #generate chart data
       @testSessions.each do |tSess|
+          #get user perception data from each test session
+          @userPercepData[0] += tSess.accuracy_ranking
+          @userPercepData[1] += tSess.ease_ranking
+          @userPercepData[2] += tSess.fun_ranking
+          @userPercepFrequency[0] += 1
+          @userPercepFrequency[1] += 1
+          @userPercepFrequency[2] += 1
+          
           #get all the test units for this particular test session
           @allTU = TestUnit.where('test_session_id = ?', tSess.id)
           @allTU.each do |tu|
@@ -57,10 +67,13 @@ class AnalyticsSpecificController < ApplicationController
               end
           end
       end
+      
       #turn totals into averages for the duration chart
       @durationChartData.each do |key, duration|
           @durationChartData[key] = ( @durationChartData[key] / @durationChartFrequency[key] )
       end
+      
+      #turn user perception rankings into averages
      
       
       render :partial => "open_keyboard_stats", :locals => { :pcData => @pieChartData, :keyboard => @keyboard, :dcData => @durationChartData } 
